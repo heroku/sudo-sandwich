4fb32d9c7afd8b076685c3ca071376e80b857dad
Jessie A. Young <jessie.young@salesforce.com>
2018-05-25 15:15:05 -0700

    Tweaks to scaffold
    * Add RSpec
    * Use pg instead of sqlite3
    * Improve setup script

---
+++ spec/rails_helper.rb
@@ -0,0 +1,16 @@
+ENV["RACK_ENV"] = "test"
+
+require File.expand_path("../../config/environment", __FILE__)
+abort("DATABASE_URL environment variable is set") if ENV["DATABASE_URL"]
+
+require "rspec/rails"
+
+Dir[Rails.root.join("spec/support/**/*.rb")].sort.each { |file| require file }
+
+RSpec.configure do |config|
+  config.infer_base_class_for_anonymous_controllers = false
+  config.infer_spec_type_from_file_location!
+  config.use_transactional_fixtures = true
+end
+
+ActiveRecord::Migration.maintain_test_schema!
---
+++ spec/spec_helper.rb
@@ -0,0 +1,17 @@
+require "webmock/rspec"
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+    mocks.verify_partial_doubles = true
+  end
+
+  config.example_status_persistence_file_path = "tmp/rspec_examples.txt"
+  config.order = :random
+end
+
+WebMock.disable_net_connect!(allow_localhost: true)

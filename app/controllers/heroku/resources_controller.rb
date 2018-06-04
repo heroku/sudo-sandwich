38a9bbaf89fe5201d698591e33b73f6c1a3faa1c
Jessie A. Young <jessie.young@salesforce.com>
2018-06-04 10:25:41 -0700

    Update config var to match manifest

--- app/controllers/heroku/resources_controller.rb
+++ app/controllers/heroku/resources_controller.rb
@@ -7,7 +7,7 @@ module Heroku
         json: {
           id: heroku_uuid,
           config: {
-            RAD_ON_ADD_ON: "tubular!"
+            RADON_ADD_ON: "tubular!"
           },
           message: "Thanks for being rad and adding the rad-on add-on."
         },

3082780e0d001d299e8c54e2df57ad8882b675e1
Jessie A. Young <jessie.young@salesforce.com>
2018-06-04 10:43:55 -0700

    update tests, too (oops)

--- spec/controllers/heroku/resources_controller_spec.rb
+++ spec/controllers/heroku/resources_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Heroku::ResourcesController do
 
       post :create
 
-      expect(response.code).to eq("202")
+      expect(response.code).to eq("200")
     end
 
     it 'returns the correct json response' do
@@ -16,7 +16,7 @@ RSpec.describe Heroku::ResourcesController do
       expected_response = {
         id: heroku_uuid,
         config: {
-          RAD_ON_ADD_ON: "tubular!"
+          RADON_ADD_ON: "tubular!"
         },
         message: "Thanks for being rad and adding the rad-on add-on."
       }

0df627fc0f06abbe8bd14f6069db65e659b31845
Jessie A. Young <jessie.young@salesforce.com>
2018-06-19 11:48:14 -0700

    Expire after 90 minutes per docs

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, expire_after: 1.hour
+Rails.application.config.session_store :cookie_store, expire_after: 90.minutes # recommended interval at https://devcenter.heroku.com/articles/add-on-single-sign-on#signing-in-the-user-on-redirect

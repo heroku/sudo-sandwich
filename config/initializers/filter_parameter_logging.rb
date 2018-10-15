b30b44771d9a5aae9f6773e0fcc813fa027bbfa7
Matt Gauger <mathias@heroku.com>
2018-10-15 15:26:16 -0500

    Clean up scrubbed fields

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -2,14 +2,12 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.filter_parameters += [
-  '_session',
+  '_session_id',
   'access_token',
+  'authenticity_token',
   'oauth_grant.code',
+  'password',
   'refresh_token',
   'request.session',
-  :access_token,
-  :authenticity_token,
-  :oauth_token,
-  :password,
-  :session
+  'session'
 ]
--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,9 +1,6 @@
 require 'rollbar/rails'
 
 Rollbar.configure do |config|
-  # Without configuration, Rollbar is enabled in all environments.
-  # To disable in specific environments, set config.enabled=false.
-
   config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
 
   # Here we'll disable in 'test' and dev:
@@ -13,31 +10,6 @@ Rollbar.configure do |config|
 
   config.environment = ENV["APP_ENVIRONMENT"] || "development"
 
-  # By default, Rollbar will try to call the `current_user` controller method
-  # to fetch the logged-in user object, and then call that object's `id`,
-  # `username`, and `email` methods to fetch those properties. To customize:
-  # config.person_method = "my_current_user"
-  # config.person_id_method = "my_id"
-  # config.person_username_method = "my_username"
-  # config.person_email_method = "my_email"
-
-  # If you want to attach custom data to all exception and message reports,
-  # provide a lambda like the following. It should return a hash.
-  # config.custom_data_method = lambda { {:some_key => "some_value" } }
-
-  # Add exception class names to the exception_level_filters hash to
-  # change the level that exception is reported at. Note that if an exception
-  # has already been reported and logged the level will need to be changed
-  # via the rollbar interface.
-  # Valid levels: 'critical', 'error', 'warning', 'info', 'debug', 'ignore'
-  # 'ignore' will cause the exception to not be reported at all.
-  # config.exception_level_filters.merge!('MyCriticalException' => 'critical')
-  #
-  # You can also specify a callable, which will be called with the exception instance.
-  # config.exception_level_filters.merge!('MyCriticalException' => lambda { |e| 'critical' })
-  #
-  # We know people are going to try to URL hack Elements (and automated
-  # whitehat testing even tries this), so ignore those type of exceptions:
   config.exception_level_filters.merge!({
     'ActionController::RoutingError' => 'ignore',
     'ActionController::UnknownHttpMethod' => 'ignore',
@@ -45,33 +17,16 @@ Rollbar.configure do |config|
     'ActionView::MissingTemplate' => 'ignore'
   })
 
-  # Enable asynchronous reporting (uses girl_friday or Threading if girl_friday
-  # is not installed)
-  # config.use_async = true
-  # Supply your own async handler:
-  # config.async_handler = Proc.new { |payload|
-  #  Thread.new { Rollbar.process_payload_safely(payload) }
-  # }
-
-  # Enable asynchronous reporting (using sucker_punch)
-  # config.use_sucker_punch
-
-  # Enable delayed reporting (using Sidekiq)
-  # config.use_sidekiq
-  # You can supply custom Sidekiq options:
-  # config.use_sidekiq 'queue' => 'my_queue'
-
   config.scrub_fields |= [
-    '_session',
+    '_session_id',
     'access_token',
+    'authenticity_token',
     'oauth_grant.code',
+    'oauth_token',
+    'password',
     'refresh_token',
     'request.session',
-    :access_token,
-    :authenticity_token,
-    :oauth_token,
-    :password,
-    :session
+    'session'
   ]
   config.scrub_headers |= [
     'Authorization',
@@ -79,7 +34,6 @@ Rollbar.configure do |config|
     'HTTP_AUTHORIZATION',
     'HTTP_X_CSRF_TOKEN',
     'X_CSRF_TOKEN',
-    'omniauth.auth',
   ]
   config.use_sucker_punch
 end
